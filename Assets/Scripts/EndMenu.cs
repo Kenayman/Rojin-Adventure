6f4a4d09358c3e6db93380e6e646635cb710ce3b
Kenneth Arguelles Camarena <kenneth.camarena7207@alumnos.udg.mx>
2023-04-13 04:24:12 -0600

    Last Update
    Last update done for the end screen

--- Assets/Scripts/EndMenu.cs
+++ Assets/Scripts/EndMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class EndMenu : MonoBehaviour
 {
+    [SerializeField] AudioSource m_AudioSource;
+    private void Awake()
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+    }
     public void EndGame()
     {
         Application.Quit();
--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Finish : MonoBehaviour
 {
-    [SerializeField] private AudioSource m_AudioSource;
+    private AudioSource m_AudioSource;
     // Start is called before the first frame update
     void Start()
     {
         m_AudioSource = GetComponent<AudioSource>();
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.name == "Player")
+        if (collision.gameObject.name == "Player")
         {
             m_AudioSource.Play();
             CompleteLevel();
@@ -21,6 +23,6 @@ public class Finish : MonoBehaviour
 
     private void CompleteLevel()
     {
-
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
