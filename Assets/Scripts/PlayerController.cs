131cb23aaadadc231982ca3a90939aa8a6ff22c1
Kenneth Arguelles Camarena <kenneth.camarena7207@alumnos.udg.mx>
2023-04-12 23:02:25 -0600

    Animation
    Basic animation for the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,12 +7,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D playerRb;
     private Animator animator;
     private SpriteRenderer sprite;
+    private BoxCollider2D coll;
 
     [SerializeField] private float jumpSpeed = 10f;
     [SerializeField] private float moveForce = 3f;
-    private bool isOnLand;
+    [SerializeField] private LayerMask jumpable;
 
-    private enum MovementState { idle, running, jumping, falling }
+    private enum MovementState { idle, running, jumping, falling, attack }
     private MovementState state = MovementState.idle;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,7 @@ public class PlayerController : MonoBehaviour
         playerRb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -32,14 +34,16 @@ public class PlayerController : MonoBehaviour
     {
         MovementState state;
 
-        if (Input.GetKeyDown(KeyCode.Space) && isOnLand==true)
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
-
             playerRb.AddForce(new Vector2(0f, jumpSpeed), ForceMode2D.Impulse);
-            
-            
-
         }
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+        }
+
+
 
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         playerRb.velocity = new Vector2(horizontalInput * 7, playerRb.velocity.y);
@@ -48,38 +52,39 @@ public class PlayerController : MonoBehaviour
         {
             state = MovementState.running;
             sprite.flipX = false;
-            isOnLand = true;
-
-
         }
 
         else if (horizontalInput < 0f)
         {
             state = MovementState.running;
             sprite.flipX = true;
-            isOnLand = true;
-
-
         }
         else
         {
             state = MovementState.idle;
-            isOnLand=true;
-
-
         }
 
         if(playerRb.velocity.y > 0.1f ) {
             state = MovementState.jumping;
-            isOnLand = false;
         }
         if (playerRb.velocity.y < -0.1f)
         {
             state = MovementState.falling;
-            isOnLand = false;
         }
 
 
+
         animator.SetInteger("state", (int)state);
+    }
+
+    private void Atack()
+    {
+            
+    }
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, 1f, jumpable);
+        
+
     }
 }
